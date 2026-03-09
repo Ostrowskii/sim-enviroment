d3fe90fd1d10f7663db94a623c778d1f14f53a44
ostrowskii <vanessaostroski08@gmail.com>
2026-03-09 13:57:47 -0400

    fix: make github pages work with branch-source fallback

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,20 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/sim-enviroment/"
+  base: "/sim-enviroment/",
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "assets/index.css";
+          }
+          return "assets/[name][extname]";
+        }
+      }
+    }
+  }
 });
